b2ba144986a0b60a94ed45de876675337935fc36
Theieyrre <crizfmtfm@gmail.com>
2020-03-24 08:28:37 +0300

    Change style

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
-var harfler = ["a","b","c","ç","d","e","f","g","h","ı","i","j","k","l","m","n","o","ö","p","r","s","ş","t","u","ü","v","y","z"];
-var i = 0;
-var last = 28;
+var harfler = ["a","b","c","ç","d","e","f","g","h","ı","i","j","k","l","m","n","o","ö","p","r","s","ş","t","u","ü","v","y","z"]
+var i = 0
+var last = harfler.length
+var kaydetBtn = document.getElementById("kaydet")
+var timerBtn = document.getElementById("timer")
+
+kaydetBtn.disabled = true;
+timerBtn.style.display = "none";
 
 function harf(){
-    i = Math.floor(Math.random() * last);
-    var h = document.getElementById("harf");
-    h.innerHTML = harfler[i];
+    i = Math.floor(Math.random() * last)
+    var h = document.getElementById("harf")
+    h.innerHTML = harfler[i]
+
+    kaydetBtn.disabled = false
+    kaydetBtn.classList.remove("done")
+    kaydetBtn.innerHTML = "Kaydet"
 }
 
 function kaydet(){
-    var temp = harfler[i];
-    harfler[i] = harfler[last-1];
-    harfler[last] = temp;
-    last = last - 1;
-    console.log(harfler);
+    var temp = harfler[i]
+    harfler[i] = harfler[last-1]
+    harfler[last-1] = temp
+    harfler.pop()
+    last = harfler.length
+    console.log(harfler)
+    
+    kaydetBtn.classList.add("done")
+    kaydetBtn.disabled = true;
+    kaydetBtn.innerHTML = "<span>✔</span> Kaydedildi"
+
+    timerBtn.style.display = "block"
 }
